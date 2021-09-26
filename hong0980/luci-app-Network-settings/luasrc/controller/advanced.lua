d5de7ae45481a0365d061f27efa834d833168733
lllrrr2 <61368564+lllrrr2@users.noreply.github.com>
2021-09-26 11:36:17 +0800

    del file

--- hong0980/luci-app-Network-settings/luasrc/controller/advanced.lua
+++
@@ -1,7 +0,0 @@
-module("luci.controller.advanced",package.seeall)
-function index()
-	-- if not nixio.fs.access("/etc/config/advanced")then return end
-	local e
-	e = entry({"admin", "system", "advanced"}, cbi("advanced"), _("快捷设置"), 71)
-	e.dependent=true
-end
