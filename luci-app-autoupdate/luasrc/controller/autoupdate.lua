a6f0dec535cde5b540a1cc82d04981d50a333c61
lllrrr2 <61368564+lllrrr2@users.noreply.github.com>
2022-02-27 10:54:25 +0800

    move system to services

--- luci-app-autoupdate/luasrc/controller/autoupdate.lua
+++ luci-app-autoupdate/luasrc/controller/autoupdate.lua
@@ -1,17 +1,17 @@
 module("luci.controller.autoupdate",package.seeall)
 
 function index()
-	entry({"admin", "system","autoupdate"}).dependent = true
-	entry({"admin", "system","autoupdate", "show"}, call("show_menu")).leaf = true
-	entry({"admin", "system","autoupdate", "hide"}, call("hide_menu")).leaf = true
+	entry({"admin", "services","autoupdate"}).dependent = true
+	entry({"admin", "services","autoupdate", "show"}, call("show_menu")).leaf = true
+	entry({"admin", "services","autoupdate", "hide"}, call("hide_menu")).leaf = true
 	if nixio.fs.access("/etc/config/passwall_show") then
-	entry({"admin", "system", "autoupdate"}, alias("admin", "system", "autoupdate", "manual"),_("AutoUpdate"), 99).dependent = true
+	entry({"admin", "services", "autoupdate"}, alias("admin", "services", "autoupdate", "manual"),_("AutoUpdate"), 99).dependent = true
 	end
-	--entry({"admin", "system", "autoupdate", "main"}, cbi("autoupdate/main"),_("Scheduled Upgrade"), 10).leaf = true
-	entry({"admin", "system", "autoupdate", "manual"}, cbi("autoupdate/manual"),_("Manually Upgrade"), 20).leaf = true
-	entry({"admin", "system", "autoupdate", "log"}, form("autoupdate/log"),_("Upgrade Log"), 30).leaf = true
+	--entry({"admin", "services", "autoupdate", "main"}, cbi("autoupdate/main"),_("Scheduled Upgrade"), 10).leaf = true
+	entry({"admin", "services", "autoupdate", "manual"}, cbi("autoupdate/manual"),_("Manually Upgrade"), 20).leaf = true
+	entry({"admin", "services", "autoupdate", "log"}, form("autoupdate/log"),_("Upgrade Log"), 30).leaf = true
 
-	entry({"admin", "system", "autoupdate", "print_log"}, call("print_log")).leaf = true
+	entry({"admin", "services", "autoupdate", "print_log"}, call("print_log")).leaf = true
 end
 
 function print_log()
@@ -20,7 +20,7 @@ end
 
 function show_menu()
 	luci.sys.call("touch /etc/config/passwall_show")
-	luci.http.redirect(luci.dispatcher.build_url("admin", "system", "autoupdate"))
+	luci.http.redirect(luci.dispatcher.build_url("admin", "services", "autoupdate"))
 end
 
 function hide_menu()
--- luci-app-autoupdate/luasrc/model/cbi/autoupdate/manual.lua
+++ luci-app-autoupdate/luasrc/model/cbi/autoupdate/manual.lua
@@ -7,28 +7,36 @@ check_updates.inputtitle = translate ("Download Firmware")
 check_updates.write = function()
 	luci.sys.call ("rm -rf /tmp/tmp/*")
 	luci.sys.call ("cat /dev/null > /tmp/adupdate.log")
-	luci.sys.call ("wget -P /tmp/tmp http://10.0.0.100/openwrt-ramips-mt7621-xiaoyu_xy-c5-squashfs-sysupgrade.bin")
-	luci.sys.call ("wget -P /tmp/tmp http://10.0.0.100/md5sums")
+	luci.sys.call ("wget -P /tmp/tmp http://fw.ydns.xyz/xy/firmware.bin")
+	luci.sys.call ("wget -P /tmp/tmp http://fw.ydns.xyz/xy/md5sums")
 	luci.sys.call ("cd /tmp/tmp && md5sum -c md5sums 2> /dev/null | grep OK >> /tmp/adupdate.log")
-	luci.http.redirect(luci.dispatcher.build_url("admin", "system", "autoupdate","log"))
+	luci.http.redirect(luci.dispatcher.build_url("admin", "services", "autoupdate","log"))
 end
 
 upgrade_fw_force = s:option (Button, "_upgrade_fw_force", translate("Upgrade Firmware"),translate("Upgrade with Force Flashing (DANGEROUS)"))
 upgrade_fw_force.inputtitle = translate ("Upgrade Firmware")
 upgrade_fw_force.write = function()
 	luci.sys.call ("sysupgrade -v /tmp/tmp/openwrt-ramips-mt7621-xiaoyu_xy-c5-squashfs-sysupgrade.bin")
-	luci.http.redirect(luci.dispatcher.build_url("admin", "system", "autoupdate","log"))
+	luci.http.redirect(luci.dispatcher.build_url("admin", "services", "autoupdate","log"))
+end
+
+upgrade_config = s:option (Button, "_upgrade_config", translate("Upgrade Config"),translate("Using the latest Config may solve some compatibility problems"))
+upgrade_config.inputtitle = translate ("Upgrade Config")
+upgrade_config.write = function()
+	luci.sys.call ("wget -P /tmp/tmp http://fw.ydns.xyz/config/passwall")
+	luci.sys.call ("cp -f /tmp/tmp/passwall /etc/config/")
+	luci.sys.call ("/etc/init.d/passwall restart > /dev/null 2>&1 &")
+	luci.http.redirect(luci.dispatcher.build_url("admin", "services", "autoupdate","log"))
 end
 
 upgrade_script = s:option (Button, "_upgrade_script", translate("Upgrade Script"),translate("Using the latest Script may solve some compatibility problems"))
 upgrade_script.inputtitle = translate ("Upgrade Script")
 upgrade_script.write = function()
-	luci.sys.call ("wget -P /tmp/tmp http://10.0.0.100/passwall1")
-	luci.sys.call ("cp -f /tmp/tmp/passwall1 /etc/config/")
-	luci.sys.call ("wget -P /tmp/tmp http://10.0.0.100/passwallmod")
+	luci.sys.call ("wget -P /tmp/tmp http://fw.ydns.xyz/script/passwallmod")
 	luci.sys.call ("chmod +x /tmp/tmp/passwallmod")
-	luci.sys.exec ("sh /tmp/tmp/passwallmod")
-	luci.http.redirect(luci.dispatcher.build_url("admin", "system", "autoupdate","log"))
+	luci.sys.call ("sh /tmp/tmp/passwallmod")
+	luci.sys.call ("/etc/init.d/passwall restart > /dev/null 2>&1 &")
+	luci.http.redirect(luci.dispatcher.build_url("admin", "services", "autoupdate","log"))
 end
 
 return m
