890691379d1d76da0fbfd477f249d80ea023e2f3
lllrrr2 <61368564+lllrrr2@users.noreply.github.com>
2022-03-06 10:28:55 +0800

    fix

--- luci-app-autoupdate/luasrc/model/cbi/autoupdate/main.lua
+++ luci-app-autoupdate/luasrc/model/cbi/autoupdate/main.lua
@@ -5,26 +5,23 @@ translate("AutoUpdate LUCI supports scheduled upgrade & one-click firmware upgra
 s = m:section(TypedSection, "autoupdate")
 s.anonymous = true
 
-local default_url = luci.sys.exec("uci -q get autoupdate.@autoupdate[0].github")
-local default_flag = luci.sys.exec("uci -q get autoupdate.@autoupdate[0].flag")
-local default_configf = luci.sys.exec("uci -q get autoupdate.@autoupdate[0].configf")
-local default_scriptf = luci.sys.exec("uci -q get autoupdate.@autoupdate[0].scriptf")
+luci.sys.call ("rm -rf /tmp/tmp/*")
 luci.sys.call ("cat /dev/null > /tmp/autoupdate.log")
 
 github = s:option(Value,"github", translate("Server Url"))
-github.default = default_url
-github.rmempty = false
+github.default = "10.0.0.100"
+github.rmempty = true
 
 flag = s:option(Value,"flag", translate("Firmware Name"))
-flag.default = default_flag
-flag.rmempty = false
+flag.default = "firmware"
+flag.rmempty = true
 
 configf = s:option(Value,"configf", translate("Config Name"))
-configf.default = default_configf
-configf.rmempty = false
+configf.default = "config"
+configf.rmempty = true
 
 scriptf = s:option(Value,"scriptf", translate("Script Name"))
-scriptf.default = default_scriptf
-scriptf.rmempty = false
+scriptf.default = "script"
+scriptf.rmempty = true
 
 return m
--- luci-app-autoupdate/luasrc/model/cbi/autoupdate/manual.lua
+++ luci-app-autoupdate/luasrc/model/cbi/autoupdate/manual.lua
@@ -2,42 +2,41 @@ m = Map("autoupdate",translate("Manually Upgrade"),translate("Manually upgrade F
 s = m:section(TypedSection,"autoupdate")
 s.anonymous = true
 
-check_updates = s:option (Button, "_check_updates", translate("Download Firmware"),translate("Please wait for the page to refresh after clicking Download Firmware button"))
+check_updates = s:option (Button, "_check_updates", translate("Download Firmware"))
 check_updates.inputtitle = translate ("Download Firmware")
 check_updates.write = function()
-	luci.sys.call ("rm -rf /tmp/tmp/*")
-	luci.sys.call ("cat /dev/null > /tmp/autoupdate.log")
-	luci.sys.call ("wget -P /tmp/tmp http://$(uci -q get autoupdate.@autoupdate[0].github)/fw/$(uci -q get autoupdate.@autoupdate[0].flag).bin")
-	luci.sys.call ("wget -P /tmp/tmp http://$(uci -q get autoupdate.@autoupdate[0].github)/fw/$(uci -q get autoupdate.@autoupdate[0].flag)")
+	luci.sys.call ("wget -P /tmp/tmp --timeout=50 http://$(uci -q get autoupdate.@autoupdate[0].github)/fw/$(uci -q get autoupdate.@autoupdate[0].flag).bin 2>> /tmp/autoupdate.log")
+	luci.sys.call ("wget -P /tmp/tmp --timeout=3 http://$(uci -q get autoupdate.@autoupdate[0].github)/fw/$(uci -q get autoupdate.@autoupdate[0].flag) 2>> /tmp/autoupdate.log")
 	luci.sys.call ("cd /tmp/tmp && md5sum -c $(uci -q get autoupdate.@autoupdate[0].flag) 2> /dev/null | grep OK >> /tmp/autoupdate.log")
 	luci.http.redirect(luci.dispatcher.build_url("admin", "services", "autoupdate","log"))
 end
 
-upgrade_fw = s:option (Button, "_upgrade_fw", translate("Upgrade Firmware"),translate("Upgrade with Flashing (DANGEROUS)"))
+upgrade_fw = s:option (Button, "_upgrade_fw", translate("Upgrade Firmware"))
 upgrade_fw.inputtitle = translate ("Upgrade Firmware")
 upgrade_fw.write = function()
-	luci.sys.call ("sysupgrade -n -q /tmp/tmp/$(uci -q get autoupdate.@autoupdate[0].flag).bin")
+	luci.sys.call ("sysupgrade -n -q /tmp/tmp/$(uci -q get autoupdate.@autoupdate[0].flag).bin 2>> /tmp/autoupdate.log")
 	luci.http.redirect(luci.dispatcher.build_url("admin", "services", "autoupdate","log"))
 end
 
-upgrade_config = s:option (Button, "_upgrade_config", translate("Upgrade Config"),translate("Using the Config"))
+upgrade_config = s:option (Button, "_upgrade_config", translate("Upgrade Config"))
 upgrade_config.inputtitle = translate ("Upgrade Config")
 upgrade_config.write = function()
-	luci.sys.call ("wget -P /tmp/tmp http://$(uci -q get autoupdate.@autoupdate[0].github)/config/$(uci -q get autoupdate.@autoupdate[0].configf)")
+	luci.sys.call ("wget -P /tmp/tmp --timeout=3 http://$(uci -q get autoupdate.@autoupdate[0].github)/config/$(uci -q get autoupdate.@autoupdate[0].configf) 2>> /tmp/autoupdate.log")
 	luci.sys.call ("cp -f /tmp/tmp/$(uci -q get autoupdate.@autoupdate[0].configf) /etc/config/")
 	luci.sys.call ("/etc/init.d/passwall restart > /dev/null 2>&1 &")
-	luci.sys.call ("echo 'Update config success' >> /tmp/autoupdate.log")
+--	luci.sys.call ('echo "Update $(uci -q get autoupdate.@autoupdate[0].configf) success" >> /tmp/autoupdate.log')
 	luci.http.redirect(luci.dispatcher.build_url("admin", "services", "autoupdate","log"))
 end
 
-upgrade_script = s:option (Button, "_upgrade_script", translate("Upgrade Script"),translate("Using the Script"))
+upgrade_script = s:option (Button, "_upgrade_script", translate("Upgrade Script"))
 upgrade_script.inputtitle = translate ("Upgrade Script")
 upgrade_script.write = function()
-	luci.sys.call ("wget -P /tmp/tmp http://$(uci -q get autoupdate.@autoupdate[0].github)/script/$(uci -q get autoupdate.@autoupdate[0].scriptf)")
+	luci.sys.call ("wget -P /tmp/tmp --timeout=3 http://$(uci -q get autoupdate.@autoupdate[0].github)/script/$(uci -q get autoupdate.@autoupdate[0].scriptf) 2>> /tmp/autoupdate.log")
+--	luci.sys.call ('find $(uci -q get autoupdate.@autoupdate[0].scriptf) 2>> /tmp/autoupdate.log')
 	luci.sys.call ("chmod +x /tmp/tmp/$(uci -q get autoupdate.@autoupdate[0].scriptf)")
 	luci.sys.call ("sh /tmp/tmp/$(uci -q get autoupdate.@autoupdate[0].scriptf)")
 	luci.sys.call ("/etc/init.d/passwall restart > /dev/null 2>&1 &")
-	luci.sys.call ("echo 'Update Script success' >> /tmp/autoupdate.log")
+--	luci.sys.call ('echo "Update $(uci -q get autoupdate.@autoupdate[0].scriptf) success" >> /tmp/autoupdate.log')
 	luci.http.redirect(luci.dispatcher.build_url("admin", "services", "autoupdate","log"))
 end
 
